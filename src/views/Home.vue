c740a007a1d7a1d52df0abe5b3fdd8d77a10ac45
GODU-LZR <2980560533@qq.com>
2025-01-13 04:59:16 +0800

    qiankun vue vue-route vue-x element-ui 图表组件 qiankun基座微应用框架整合

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<!-- vue-qk-finance/src/views/Home.vue -->
+<template>
+  <div>
+    <h2>体育开支模块首页</h2>
+    <el-button type="primary" @click="showMessage">点击测试</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FinanceHome',
+  methods: {
+    showMessage() {
+      this.$message('体育开支子应用的 ElementUI 按钮点击了！');
+    }
+  }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
+// vue-qk-finance/vue.config.js
 const { defineConfig } = require('@vue/cli-service')
+const { name } = require('./package.json')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/finance/' : '/',
+  devServer: {
+    port: 3007, // 和上面主应用区分
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      chunkLoadingGlobal: `webpackJsonp_${name}`
+    }
+  }
 })
+
